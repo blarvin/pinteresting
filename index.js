8710e43a73a76edc12c19d61457bb855c2e3ff8c
blarvin <blarvinius@gmail.com>
2022-09-25 07:35:25 +0100

    add HELLO message

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ document.addEventListener("DOMContentLoaded", () => {
 
   // create pin image url input
   const imageUrl = document.createElement("input");
-  imageUrl.placeholder = "Image URL";
+  imageUrl.placeholder = "HELLO ELEANORA";
   imageUrl.type = "url";
   imageUrl.required = true;
 
